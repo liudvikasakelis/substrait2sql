6af1e60591cf7c4b1e7a1ae032aa4bf621907f4f
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2024-04-16 22:08:04 +0300

    Filter doesn't work yet!

--- tests/simple_filter.py
+++ tests/simple_filter.py
@@ -3,10 +3,9 @@ from ibis import _
 
 from common import run_test, sqlite_con
 
-
 table1 = sqlite_con.table("table1")
 
-def transformation(x):
-    return(x.select(_.id, _.ech))
+def transformation(table):
+    return(table.filter(_.id < 4))
 
 print(run_test(transformation, table1))
